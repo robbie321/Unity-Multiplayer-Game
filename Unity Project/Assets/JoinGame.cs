92b7edbc4bef4c69fd3e68a06bf06b183b7e0379
Robbie Malone <robmalone7@gmail.com>
2018-09-12 15:05:18 +0100

    added hosting GUI

---
+++ Unity Project/Assets/JoinGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class JoinGame : MonoBehaviour {
+    private NetworkManager networkManager;
+    private List<GameObject> roomList = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RefreshRoomList()
+    {
+
+    }
+}
--- Unity Project/Assets/Scripts/PlayerSetup.cs
+++ Unity Project/Assets/Scripts/PlayerSetup.cs
@@ -14,12 +14,14 @@ public class PlayerSetup : NetworkBehaviour {
         {
             for (int i = 0; i < componentsToDiasble.Length; i++)
             {
+                transform.position = new Vector3(17, 1, 10);
                 componentsToDiasble[i].enabled = false;
             }
 
         }
         else
         {
+            transform.position = new Vector3(17, 1, -10);
             sceneCamera = Camera.main;
             if(sceneCamera != null)
             {
