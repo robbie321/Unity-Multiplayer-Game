d19ce74dff8c739bcd1070bdec9b66d1dc6bfe6c
Darran Blackweir <darran.blacky@gmail.com>
2018-09-15 03:05:17 +0100

    quick fix

--- Unity Project/Assets/Scripts/Player.cs
+++ Unity Project/Assets/Scripts/Player.cs
@@ -87,27 +87,11 @@ public class Player : NetworkBehaviour
         yield return new WaitForSeconds(GameManager.instance.matchSettings.respawnTime);
 
         SetDefaults();
-<<<<<<< HEAD
-        if (!isLocalPlayer)
-        {
-            transform.position = new Vector3(17, 1, 10);
-            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-        }
-        else
-        {
-            transform.position = new Vector3(17, 1, -10);
-            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-        }
-        //Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
-        //transform.position = _spawnPoint.position;
-        //transform.rotation = _spawnPoint.rotation;
-=======
         //Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
 
         Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
         transform.position = _spawnPoint.position;
         transform.rotation = _spawnPoint.rotation;
->>>>>>> master
 
         Debug.Log(transform.name + " respawned.");
     }
